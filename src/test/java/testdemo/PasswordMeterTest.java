fccc39341ef0776d5ab40d3e0ea46817cd4dae2b
taeukyoon <79095857+taeukyoon@users.noreply.github.com>
2022-07-25 21:37:02 +0900

    refactor: passwordMeter 필드값 추가

--- src/test/java/testdemo/PasswordMeterTest.java
+++ src/test/java/testdemo/PasswordMeterTest.java
@@ -7,17 +7,19 @@ import org.junit.jupiter.api.Test;
 
 public class PasswordMeterTest {
 
+	private PasswordMeter passwordMeter = new PasswordMeter();
+
 	@DisplayName("null 입력하면 예외 발생")
 	@Test
 	void nullInput() {
 		assertThatIllegalArgumentException()
-			.isThrownBy(() -> new PasswordMeter().meter(null));
+			.isThrownBy(() -> passwordMeter.meter(null));
 	}
 
 	@DisplayName("공백을 입력하면 예외 발생")
 	@Test
 	void blankInput() {
 		assertThatIllegalArgumentException()
-			.isThrownBy(() -> new PasswordMeter().meter(""));
+			.isThrownBy(() -> passwordMeter.meter(""));
 	}
 }
